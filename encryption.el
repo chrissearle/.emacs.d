cc64eb764ec428b320889728ce9f636fd4d5af88
Chris Searle <chris@chrissearle.org>
2011-12-15 12:59:53 +0100

    Support partial org-mode encryption too

--- encryption.el
+++ encryption.el
@@ -1,4 +1,9 @@
 (require 'epa-file)
 (epa-file-enable)
 
+(require 'org-crypt)
+(org-crypt-use-before-save-magic)
+(setq org-tags-exclude-from-inheritance (quote ("crypt")))
+(setq org-crypt-key "D4BF0A41")
+
 (provide 'encryption)
